49a1e14673159e98fa4435491937ba0f36c1e3b4
badsoap141 <badsoap141@gmail.com>
2023-02-26 15:56:16 +0900

    Feat : [DEV-TEST] 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
 import "./App.css";
 
 function App() {
+  const dummy = [
+    {
+      id: 1,
+      createdAt: "timeasdkasld",
+      likes: 3,
+      pallete: [
+        {
+          id: 1,
+          code: "4E6E81",
+        },
+        {
+          id: 2,
+          code: "4E6E81",
+        },
+        {
+          id: 3,
+          code: "4E6E81",
+        },
+        {
+          id: 4,
+          code: "4E6E81",
+        },
+      ],
+    },
+    {
+      id: 2,
+      pallete: [
+        {
+          id: 1,
+          code: "4E6E81",
+        },
+        {
+          id: 2,
+          code: "4E6E81",
+        },
+        {
+          id: 3,
+          code: "4E6E81",
+        },
+        {
+          id: 4,
+          code: "4E6E81",
+        },
+      ],
+    },
+  ];
   return <div className="App"></div>;
 }
 
