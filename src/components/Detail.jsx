073852e4191c6257445f659ea56bf6e5cae7a9da
sehwa <love7620640@naver.com>
2023-03-12 10:56:00 +0900

    feature : [DEV-002] collection 및 detail 페이지 추가

---
+++ src/components/Detail.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import ItemComponent from "./ItemComponent";
+import { Routes, Route, useParams } from "react-router-dom";
+
+export default function Detail({ color }) {
+  return (
+    <Container>
+      <Content>
+        <ItemWrapper>
+          <ItemComponent color={color} w="440px" h="440px" />
+          <Title>{color}</Title>
+        </ItemWrapper>
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  margin: 60px auto;
+  font-size: 20px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 815px;
+  margin: 50px auto;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  flex-direction: column;
+`;
